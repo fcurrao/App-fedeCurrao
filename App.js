65821286e32c3cdd67025cface9034acc5959258
fcurrao <fcurrao@gmail.com>
2023-06-15 21:34:41 -0300

    agrege input y buton

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>My ecomerce</Text>
+      <TextInput
+        editable 
+        numberOfLines={4}
+        maxLength={40}  
+        
+        style={{paddingHorizontal: 100,backgroundColor: 'gray'}}
+      />
+      <Button title="agregar">Agregar</Button>
       <StatusBar style="auto" />
     </View>
   );
